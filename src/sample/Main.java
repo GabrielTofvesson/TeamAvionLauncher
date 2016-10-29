e1210440f45f61844ddf61c1e9f5a1153ea8309f
Michael-Jouanneau <tacomundu12@gmail.com>
2016-10-29 21:07:04 +0200

    Added a rectangle to play with

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,23 +16,18 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         primaryStage.initStyle(StageStyle.UNDECORATED);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Team Avion Launcher [WIP]");
         primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
         primaryStage.maximizedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue)
                 primaryStage.setMaximized(false);
         });
-        root.lookup("#exit").setOnMouseClicked(event -> primaryStage.close());                                          // Close program if button is clicked
-
-
-
-
+        root.lookup("#exit").setOnMouseClicked(event -> primaryStage.close());
 
         // Drag
-        root.lookup("#dank").setOnMouseDragged(event -> {
-            primaryStage.setX(event.getScreenX());
-            primaryStage.setY(event.getScreenY());
+        root.lookup("#rectangle").setOnMouseDragged(event -> {
+
         });
 
     }
