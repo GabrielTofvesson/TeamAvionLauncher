d7340c255df009b788ba4da344c61f04a039fc9d
Michael-Jouanneau <tacomundu12@gmail.com>
2016-10-29 20:20:21 +0200

    Added no Fullscreen code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,12 +11,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.initStyle(StageStyle.UNIFIED);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
-
+        primaryStage.maximizedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue)
+                primaryStage.setMaximized(false);
+        });
         root.lookup("#exit").setOnMouseClicked(event -> primaryStage.close());                                          // Close program if button is clicked
     }
 
